b623f63b314b95d89c67a6adaea159ad92e6e3f2
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-07 12:14:45 -0600

    Initial commit

---
+++ src/process/controller/ProcessController.java
@@ -0,0 +1,10 @@
+package process.controller;
+
+public class ProcessController
+{
+	public void start()
+	{
+		System.out.println("This is a practice project. ");
+		System.out.println("By practicing I develop muscle memory :D");
+	}
+}
---
+++ src/process/controller/ProcessRunner.java
@@ -0,0 +1,10 @@
+package process.controller;
+
+public class ProcessRunner
+{
+	public static void main (String [ ] args)
+	{
+		ProcessController appController = new ProcessController();
+		appController.start();
+	}
+}
